ae2c08d37b6dffb69129b821d9133979dd683cad
Thallesttt <56350385+Thallesttt@users.noreply.github.com>
2020-06-18 23:58:35 -0300

    teste do arquivo em javascript
    botando um arquivo teste que será editado

---
+++ javascript/funções.js
@@ -0,0 +1,21 @@
+
+//game[0]= mario bros
+//game[1]= mario bros 2
+//game[2]= mario bros 3
+//game[3]= new super mario bros 
+//game[4]= super mario 64
+//game[5]= super mario galaxy
+//game[6]= super mario galaxy 2
+//game[7]= super mario 3d land 
+//game[8]= super mario 3d world
+//game[9]= super mario odyssey
+function show(){
+  var select=[10];
+  select[0]= game[0];
+  select[1]= game[0];
+  select[2]= game[0];
+  select[3]= game[0];
+  select[4]= game[0];
+  select[5]= game[0];
+  select[6]= game[0];  
+}
